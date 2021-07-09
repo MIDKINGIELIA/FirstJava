c4b02eaac16250106638f68c4eb00bcbb5160546
robot <robot@208-pro4-2>
2021-07-09 10:58:26 +0900

    첫번째 Git Test

---
+++ FIrstGitJava/src/s20409_0709/GitJava.java
@@ -0,0 +1,11 @@
+package s20409_0709;
+
+public class GitJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("로봇고 2학년 4반 9번 박기범입니다.");
+	}
+
+}
